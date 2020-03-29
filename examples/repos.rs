e2d6c842b319bbb7765d350de42a11394b3f7036
Victor Palade <victor@cloudflavor.io>
2020-03-29 21:08:52 +0200

    example: update example to match the latest implementation.

--- examples/repos.rs
+++ examples/repos.rs
@@ -6,12 +6,13 @@ use bitbucket::prelude::*;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let _client = Client::new(
+    let client = Client::new(
         "my_token".to_string(),
-        false,
-        false,
         "http://bitbucket.company.com".to_string(),
+        false,
+        false,
     );
-    let _proj = Project::new();
+    let proj = Project::new(client);
+    proj.get("my_repo").await.unwrap();
     Ok(())
 }
