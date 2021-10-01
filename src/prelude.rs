ba58ce776668df1907145cdce8f9b5d1691cf794
Victor Palade <victor@cloudflavor.io>
2021-10-01 10:21:50 +0200

    reinit: remove old files.
    Signed-off-by: Victor Palade <victor@cloudflavor.io>

--- src/prelude.rs
+++
@@ -1,8 +0,0 @@
-//! Prelude exports the main library types for import convenience.
-pub use crate::resources::comments::Comment;
-pub use crate::resources::projects::Project;
-pub use crate::resources::pullrequests::PullRequest;
-pub use crate::resources::repositories::Repository;
-pub use crate::resources::webhooks::WebHook;
-
-pub(crate) type Result<T> = std::result::Result<T, std::io::Error>;
--- src/resources/mod.rs
+++
@@ -1,6 +0,0 @@
-//! Resources incapsulates all the Bitbucket server resources.
-pub mod comments;
-pub mod projects;
-pub mod pullrequests;
-pub mod repositories;
-pub mod webhooks;
